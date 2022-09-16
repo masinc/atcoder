5259538c0dd3337271bdbb6ade6f53f881140618
masinc <masinc000@gmail.com>
2022-09-17 07:50:39 +0900

    answer 28

--- math-and-algorithm/src/bin/028.rs
+++ math-and-algorithm/src/bin/028.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        h: [isize; n]
+    }
+
+    let mut dp = Vec::with_capacity(n);
+    dp.push(0);
+    dp.push(isize::abs(h[1] - h[0]));
+
+    for i in 2..n {
+        let p1 = isize::abs(h[i - 1] - h[i]) + dp[i - 1];
+        let p2 = isize::abs(h[i - 2] - h[i]) + dp[i - 2];
+
+        dp.push(isize::min(p1, p2));
+    }
+    println!("{}", dp.last().unwrap())
 }
