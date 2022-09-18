2eec463a71fae17a1fe9ec1a34f157f1d7c3801e
masinc <masinc000@gmail.com>
2022-09-18 22:25:08 +0900

    answer 038

--- math-and-algorithm/src/bin/038.rs
+++ math-and-algorithm/src/bin/038.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        a: [usize; n],
+        v: [(usize, usize); q]
+    }
+
+    let b = {
+        let mut bb = Vec::with_capacity(n + 1);
+        bb.push(0);
+
+        bb.extend(a.iter().scan(0, |state, &x| {
+            *state += x;
+            Some(*state)
+        }));
+        bb
+    };
+
+    for (x, y) in v {
+        println!("{}", b[y] - b[x - 1]);
+    }
 }
