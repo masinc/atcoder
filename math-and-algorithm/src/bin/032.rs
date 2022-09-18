aa6505c8d24b2a4f7969b35792d95c688c3c6d0d
masinc <masinc000@gmail.com>
2022-09-18 21:06:18 +0900

    answer 032

--- math-and-algorithm/src/bin/032.rs
+++ math-and-algorithm/src/bin/032.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
+#[inline]
+fn contains(a: &[usize], x: usize) -> bool {
+    let len = a.len();
+    let mid = len / 2;
+
+    if a[mid] == x {
+        true
+    } else if len == 1 {
+        false
+    } else if a[mid] < x {
+        contains(&a[mid..], x)
+    } else if a[mid] > x {
+        contains(&a[..mid], x)
+    } else {
+        unreachable!();
+    }
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        mut a: [usize; n]
+    }
+
+    a.sort();
+    let r = if contains(&a, x) { "Yes" } else { "No" };
+    println!("{}", r);
 }
