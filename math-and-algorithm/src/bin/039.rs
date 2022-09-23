83f262f5a0459230f6787d27e2bbd7e060cd3681
masinc <masinc000@gmail.com>
2022-09-23 18:33:28 +0900

    answer 039

--- math-and-algorithm/src/bin/039.rs
+++ math-and-algorithm/src/bin/039.rs
@@ -1,3 +1,36 @@
+use itertools::Itertools;
+use proconio::{fastout, input};
+use std::cmp::Ordering;
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        v: [(usize, usize, isize); q]
+    };
+
+    let mut memo = vec![0; n + 1];
+
+    for (l, r, x) in v {
+        *memo.get_mut(l - 1).unwrap() += x;
+        *memo.get_mut(r).unwrap() -= x;
+    }
+
+    let mut re: Vec<_> = memo
+        .into_iter()
+        .scan(0, |acc, x| {
+            *acc += x;
+            Some(*acc)
+        })
+        .tuple_windows()
+        .map(|(a, b)| match a.cmp(&b) {
+            Ordering::Equal => "=",
+            Ordering::Greater => ">",
+            Ordering::Less => "<",
+        })
+        .collect();
+
+    re.pop();
+    println!("{}", re.join(""))
 }
