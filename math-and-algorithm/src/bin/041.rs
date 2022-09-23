00fdda716fafeec3e38757eefd7a461ad16b65ac
masinc <masinc000@gmail.com>
2022-09-24 05:04:02 +0900

    answer 041

--- math-and-algorithm/src/bin/041.rs
+++ math-and-algorithm/src/bin/041.rs
@@ -1,3 +1,29 @@
+use itertools::Itertools;
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        t: usize,
+        n: usize,
+        v: [(usize, usize); n]
+    }
+
+    let mut memo = vec![0; t + 1];
+    for (l, r) in v {
+        memo[l] += 1;
+        memo[r] -= 1;
+    }
+    memo.pop();
+
+    let r = memo
+        .iter()
+        .scan(0, |acc, x| {
+            *acc += x;
+            Some(*acc)
+        })
+        .map(|x| x.to_string())
+        .collect_vec();
+
+    println!("{}", r.join("\n"))
 }
